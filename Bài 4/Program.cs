077f48455b04f9fd178e54e8261d198872cc13c8
DatPTPD11090 <Phantuandat91203@gmail.com>
2024-05-11 20:27:22 +0700

    LAB 1

---
+++ Bài 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student
+{
+    public string Name;
+    public int Age;
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+         
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> studentlist = new List<Student>();
+
+        studentlist.Add(new Student("Alice", 20));
+        studentlist.Add(new Student("Bob", 22));
+        studentlist.Add(new Student("Charlie", 21));
+
+        foreach (var student in studentlist)
+        {
+            student.DisplayInfo();
+        }
+    }
+}
